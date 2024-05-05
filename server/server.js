7f7739ecfb76e0ea7e66a8a894da58a5b476f2ea
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-05-05 22:33:33 +0530

    update CORS headers

--- server/server.js
+++ server/server.js
@@ -7,6 +7,19 @@ import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://nasa-api-client.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 connectDB();
 
 const app = express();
