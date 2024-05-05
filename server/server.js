746b3af4fae050b6f06a367c859159e07a6d75f3
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-05-05 22:45:09 +0530

    changed header url

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://nasa-api-client.onrender.com"
+    "https://6637b881456725f2c9a1a6d2--symphonious-sunflower-235e5c.netlify.app"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
